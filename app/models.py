b173ccbb3d4ff0fb0fa718c8133e50b9db5a9e83
El Amraoui Khalil <khalilelam16@gmail.com>
2024-03-25 20:02:16 +0000

    modified models

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
    name = models.CharField(max_length=255)
-   price = models.FloatField()
+   quantity = models.IntegerField()
    created = models.DateTimeField(auto_now_add=True)
    updated = models.DateTimeField(auto_now=True)
 
